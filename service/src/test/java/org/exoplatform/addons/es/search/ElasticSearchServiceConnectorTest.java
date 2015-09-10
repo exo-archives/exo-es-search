0a4894478e9e660a11b7c259c9cbfdadbecb5302
Benoit de Chateauvieux <bdechateauvieux@exoplatform.com>
2015-09-10 10:14:42 +0700

    add test ElasticSearchServiceConnectorTest

---
+++ service/src/test/java/org/exoplatform/addons/es/search/ElasticSearchServiceConnectorTest.java
@@ -0,0 +1,10 @@
+package org.exoplatform.addons.es.search;
+
+/**
+ * Created by The eXo Platform SAS
+ * Author : eXoPlatform
+ * exo@exoplatform.com
+ * 9/9/15
+ */
+public class ElasticSearchServiceConnectorTest {
+}
