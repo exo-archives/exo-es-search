c766cd4821d4379df09871aa5498b32d0c79d2e6
thibaultclem <tclement@exoplatform.com>
2015-10-13 17:03:57 +0700

    Enhance Indexing Management page

--- webapp/src/main/java/org/exoplatform/addons/es/assets/javascripts/controller.js
+++ webapp/src/main/java/org/exoplatform/addons/es/assets/javascripts/controller.js
@@ -5,6 +5,36 @@
 
 var indexingManagementController = angular.module('indexingManagementController', []);
 
+indexingManagementController.controller('IndexingStatCtrl', ['$scope', '$interval', 'Stat',
+    function($scope, $interval, Stat) {
+
+        //Loads and populates the stats
+        this.loadStats = function (){
+
+            Stat.getNbConnectors().success(function(data) {
+                $scope.nbConnectors = data;
+            });
+
+            Stat.getNbIndexingOperation().success(function(data) {
+                $scope.nbOperations = data;
+            });
+
+            Stat.getNbIndexingError().success(function(data) {
+                $scope.nbErrors = data;
+            });
+
+        }
+        //Put in interval, first trigger after 5 seconds
+        $interval(function(){
+            this.loadStats();
+        }.bind(this), 5000);
+
+        //invoke initialy
+        this.loadStats();
+
+    }
+]);
+
 indexingManagementController.controller('ConnectorListCtrl', ['$scope', 'Connector',
     function($scope, Connector) {
 
@@ -30,4 +60,37 @@ indexingManagementController.controller('ConnectorListCtrl', ['$scope', 'Connect
             });
         };
 
-    }]);
+    }
+]);
+
+indexingManagementController.controller('ErrorListCtrl', ['$scope', 'Error',
+    function($scope, Error) {
+
+        Error.getErrors().success(function(data) {
+            $scope.errors = data;
+        });
+
+        $scope.addToQueue = function(id) {
+            Error.addToQueue(id).then(function(response) {
+                console.log("AddToQueue request response = "+response)
+            });
+        };
+
+    }
+]);
+
+indexingManagementController.controller('OperationListCtrl', ['$scope', 'Operation',
+    function($scope, Operation) {
+
+        Operation.getOperations().success(function(data) {
+            $scope.operations = data;
+        });
+
+        $scope.deleteOperation = function(id) {
+            Operation.deleteOperation(id).then(function(response) {
+                console.log("deleteOperation request response = "+response)
+            });
+        };
+
+    }
+]);
--- webapp/src/main/java/org/exoplatform/addons/es/assets/javascripts/service.js
+++ webapp/src/main/java/org/exoplatform/addons/es/assets/javascripts/service.js
@@ -5,25 +5,70 @@
 'use strict';
 
 var indexingManagementServices = angular.module('indexingManagementServices', []);
+var urlBase = '/rest/indexingManagement';
+
+indexingManagementServices.service('Stat', ['$http', function ($http) {
+
+    this.getNbConnectors = function () {
+        return $http.get(urlBase + '/connector/_count');
+    };
+
+    this.getNbIndexingOperation = function() {
+        return $http.get(urlBase + '/operation/_count');
+    };
+
+    this.getNbIndexingError = function() {
+        return $http.get(urlBase + '/error/_count');
+    };
+
+}]);
 
 indexingManagementServices.service('Connector', ['$http', function ($http) {
 
-    var urlBase = '/rest/indexingManagement';
+    var urlConnectorBase = urlBase + '/connector';
 
     this.getConnectors = function () {
-        return $http.get(urlBase + '/connector');
+        return $http.get(urlConnectorBase);
     };
 
     this.reindexConnector = function(connectorType) {
-        return $http.get(urlBase + '/connector/' + connectorType + '/_reindex');
+        return $http.get(urlConnectorBase + '/' + connectorType + '/_reindex');
     };
 
     this.disableIndexConnector = function(connectorType) {
-        return $http.get(urlBase + '/connector/' + connectorType + '/_disable');
+        return $http.get(urlConnectorBase + '/' + connectorType + '/_disable');
     };
 
     this.enableIndexConnector = function(connectorType) {
-        return $http.get(urlBase + '/connector/' + connectorType + '/_enable');
+        return $http.get(urlConnectorBase + '/' + connectorType + '/_enable');
+    };
+
+}]);
+
+indexingManagementServices.service('Error', ['$http', function ($http) {
+
+    var urlErrorBase = urlBase + '/error';
+
+    this.getErrors = function () {
+        return $http.get(urlErrorBase);
+    };
+
+    this.addToQueue = function(id) {
+        return $http.post(urlErrorBase + '/' + id + '/_addToQueue');
+    };
+
+}]);
+
+indexingManagementServices.service('Operation', ['$http', function ($http) {
+
+    var urlOperationBase = urlBase + '/operation';
+
+    this.getOperations = function () {
+        return $http.get(urlOperationBase);
+    };
+
+    this.deleteOperation = function(id) {
+        return $http.delete(urlOperationBase + '/' + id);
     };
 
 }]);
